7757b778bc815e9d1109ac23ae126ff56736c887
Sven Assmann <sven@d34dl0ck.me>
2017-10-18 23:45:47 +0200

    refinements

--- src/Main.php
+++ src/Main.php
@@ -7,7 +7,7 @@ require __DIR__ . '/../vendor/autoload.php';
 $bill = new Bill();
 $bill->addProduct(new Hamburger());
 $bill->addProduct(new Cheeseburger(new Onion()));
-$bill->addProduct(new Cheeseburger(new Cheese(new Cheese())));
+$bill->addProduct(new Cheeseburger(new Cheese()));
 
 printf("==============================\n");
 printf("Total is %2.2f\n", $bill->getTotal());
